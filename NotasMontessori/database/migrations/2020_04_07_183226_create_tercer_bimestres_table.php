22a0817c796150ba9903d1a2849c1b1895891ce0
Mario <mpachecoa2@miumg.edu.gt>
2020-04-07 13:03:44 -0600

    implementation database

---
+++ NotasMontessori/database/migrations/2020_04_07_183226_create_tercer_bimestres_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTercerBimestresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tercer_bimestres', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('alumno_id')->unsigned();
+            $table->foreign('alumno_id')->references('id')->on('alumnos');
+            
+            $table->bigInteger('clase_id')->unsigned();
+            $table->foreign('clase_id')->references('id')->on('clases');
+
+            $table->string('nota');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tercer_bimestres');
+    }
+}
